2abdd52fb192211311d9dfc43664c3af8a8207d1
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2024-08-21 12:55:43 +0500

    Card debuging

--- src/Components/Modals/StudentCard.js
+++ src/Components/Modals/StudentCard.js
@@ -1,18 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import html2canvas from "html2canvas";
 import { Button } from "@chakra-ui/react";
 import jsPDF from "jspdf";
 
 const StudentCard = ({ student, qrCode }) => {
-    const [qrCodeLoaded, setQrCodeLoaded] = useState(false);
-
-    const handleQrCodeLoad = () => {
-        setQrCodeLoaded(true);
-    };
+    console.log(student, qrCode);
 
     const captureAndDownload = async () => {
-        if (!qrCodeLoaded) return;
-
         const cardFront = document.querySelector(".card-front");
         const cardBack = document.querySelector(".card-back");
 
@@ -38,13 +32,11 @@ const StudentCard = ({ student, qrCode }) => {
     };
 
     const generatePDF = async () => {
-        if (!qrCodeLoaded) return;
-
         const cardFront = document.querySelector(".card-front");
         const cardBack = document.querySelector(".card-back");
 
         // Capture front and back of the card
-        const frontCanvas = await html2canvas(cardFront, { scale: 5 });
+        const frontCanvas = await html2canvas(cardFront, { scale: 5, });
         const backCanvas = await html2canvas(cardBack, { scale: 5 });
 
         // Create jsPDF document
@@ -69,7 +61,47 @@ const StudentCard = ({ student, qrCode }) => {
         <>
             <div style={{ justifyContent: "center", alignItems: "center", display: "flex" }}>
                 <div className="card-front" style={{ display: "grid", width: "288px" }}>
-                    {/* Front card content here */}
+                    <div className="relative w-72 h-56 bg-black p-6">
+                        <img src="/card-logo.png" className="w-36 mx-auto" alt="random" />
+                    </div>
+                    <div className="relative w-72 h-20 bg-white">
+                        <img
+                            src={student.image || "/17698878.jpg"}
+                            style={{ border: "4px solid goldenrod" }}
+                            className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-36"
+                            alt="Student"
+                        />
+                    </div>
+                    <div className="relative w-72 h-48 p-5 bg-white">
+                        <table style={{ textAlign: "left" }}>
+                            <tbody>
+                                <tr>
+                                    <th>Name:</th>
+                                    <td>
+                                        <u>{student.name || "N/A"}</u>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>S/O:</th>
+                                    <td>
+                                        <u>{student.father_name || "N/A"}</u>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>Batch:</th>
+                                    <td>
+                                        <u>{student.batch.name || "N/A"}</u>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div className="w-72 h-9 p-2" style={{ backgroundColor: "goldenrod" }}>
+                        <p className="text-center text-black font-bold">
+                            0331-000-111-0 / 0333-9800938
+                        </p>
+                    </div>
                 </div>
                 <div
                     className="card-back ml-4"
@@ -90,24 +122,62 @@ const StudentCard = ({ student, qrCode }) => {
                         </p>
                     </div>
                     <div className="relative w-72 h-36 p-10 bg-white">
-                        {/* Back card content */}
+                        <table style={{ textAlign: "center" }}>
+                            <tbody>
+                                <tr>
+                                    <th>Issued on:</th>
+                                    <td>
+                                        <u>{new Date().toLocaleDateString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit' }).split('/').join('-')}</u>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>Valid Till:</th>
+                                    <td>
+                                        <u>{new Date(student.batch.enddate).toLocaleDateString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit' }).split('/').join('-')} </u>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div className="w-72 h-9 p-.5" style={{ backgroundColor: "goldenrod" }}>
+                        <p className="text-center text-black font-bold text-xl">
+                            INSTRUCTIONS
+                        </p>
                     </div>
                     <div className="relative w-72 h-64 bg-white" style={{ paddingTop: "5px", overflow: "hidden" }}>
-                        {/* Instructions */}
+                        <div style={{
+                            fontSize: "12px",
+                            fontFamily: "calibri",
+                            listStyleType: "decimal",
+                            padding: "20px",
+                        }}>
+                            <h6>1. This card is property of Lahore CSS Academy.</h6>
+                            <h6>2. This card is for personal use only and is not</h6>
+                            <h6 style={{ marginLeft: "13px" }}>transferable.</h6>
+                            <h6>3. If found please return to the below address.</h6>
+                        </div>
+
                         <div className="absolute bottom-1 left-1" style={{ paddingBottom: "20px" }}>
-                            <img
-                                src={`data:image/png;base64,${qrCode}`}
-                                alt="QR Code"
-                                className="w-28 h-28"
-                                onLoad={handleQrCodeLoad}
-                            />
+                            <img src={`data:image/png;base64,${qrCode}`} alt="QR Code" className="w-28 h-28" />
                         </div>
                         <div className="absolute bottom-2 right-2 text-right">
                             <hr className="border-t-2 border-black mb-1" />
                             <span className="font-semibold">Issuing Authority</span>
                         </div>
                     </div>
-                    {/* Address */}
+                    <div className="w-72 h-9 p-.5" style={{ backgroundColor: "goldenrod" }}>
+                        <p style={{
+                            fontSize: "11px",
+                            fontFamily: "calibri",
+                            textAlign: "center",
+                            color: "black",
+                            fontWeight: "bold",
+                            padding: "6px",
+                        }}>
+                            13- Sher Shah, New Garden Town, Barkat Market, Lahore
+                        </p>
+                    </div>
                 </div>
             </div>
             <div style={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}>
